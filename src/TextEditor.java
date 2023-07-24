15942ebdded4d98946212066f16b5fccd259b27e
khushboogupta2001 <138224225+khushboogupta2001@users.noreply.github.com>
2023-07-24 23:35:38 +0530

    Update TextEditor.java

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,14 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ronak
- */
-
-    import java.awt.*;
+import java.awt.*;
 import java.awt.FileDialog;
 import java.awt.datatransfer.*;
 import java.awt.datatransfer.Clipboard;
